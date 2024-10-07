dcfddc7dea5433d251d0390e8648ab39e9f752c3
julieandersson <juan2301@student.miun.se>
2024-10-07 10:12:31 +0200

    lagt till express, mongoose och CORS. Konfigurerat anslutning till MongoDB

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express"); // Inkluderar express
+const app = express(); // Använder express
+
+const mongoose = require("mongoose"); // Inkluderar mongoose
+
+// Inkluderar och använder cors för att tillåta alla domäner
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config(); // Inkluderar env-fil
+
+app.use(express.json()); // Middleware för konvertering till json
+const port = process.env.PORT || 3000;
+
+// Ansluter till MongoDB med URL från env-filen
+mongoose.set("strictQuery", false); // Använder inte strikt sökning
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Ansluten till MongoDB!");
+}).catch((error) => {
+    console.error("Fel vid anslutning till databasen...");
+});
+
+// Startar applikationen
+app.listen(port, () => {
+    console.log("Servern startad på port: " + port);
+});
